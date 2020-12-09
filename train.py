44c9cb943e51f40e056698de3dc16dc762b4a696
Ubuntu <ubuntu@ip-172-31-43-46.ap-northeast-1.compute.internal>
2020-12-09 18:07:58 +0000

    train

--- train.py
+++ train.py
@@ -6,20 +6,22 @@ import torch
 from  torchfm.model.afn import AdaptiveFactorizationNetwork
 from torch.utils.data import DataLoader
 from dataloader import TrainDataset
+from sklearn.metrics import mean_absolute_error
 
 
 def get_args():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--input_csv', type=str, default='')
+    parser.add_argument('--input_csv', type=str, default='./out.csv')
     parser.add_argument('--batch_size', type=int, default=20)
 
     parser.add_argument('--embed_dims', type=int, default=16)
     parser.add_argument('--LNN_dim', type=int, default=1500)
         
     parser.add_argument('--lr', type=float, default=0.001)
-    
+    parser.add_argument('--epochs', type=int, default=20)
+
     args = parser.parse_args()
     return args
 
@@ -27,36 +29,68 @@ def get_args():
 def train_one_epoch(model, loader, criterion, device, optimizer):
 
     model.train()
-
+    model.to(device)
+    
     for i, (fields, targets) in enumerate(loader):
         
-        fields, targets = fields.to(device), fields.to(device)
+        fields, targets = fields.to(device), targets.to(device)
         y = model(fields)
-
-        loss = criterion(targets.float(), y)
+        
+        loss = criterion(y, targets.float())
 
         model.zero_grad()
         loss.backward()
         optimizer.step()
 
+        
 def validate_model(model, loader, device):
 
     model.to(device)
+    model.eval()
 
+    y_list, x_list = list(), list()
+    
     for i, (fields, targets) in enumerate(loader):
-
-        y = model(fields)
         
+        y = model(fields)
 
+        x_list.extends(y.to_list())
+        y_list.extends(targets.to_list())
+        
+    x_list = np.array(x_list)
+    y_list = np.array(y_list)
+
+    print ('mean absolute error')
+    print (mean_absolute_error(y_list, x_list))
+
+        
+        
 if __name__ == '__main__':
 
     args = get_args()
-    
-    model = AdaptiveFactorizationNetwork(field_dims=22, embed_dims=args.embed_dims, LNN_dim=args.LNN_dim,
+
+    field_dims = [49, 50, 50, 7, 8, 20, 20, 20, 20, 20, 20, 20, 50, 50, 20, 20, 10, 10, 5, 3, 3, 5]
+    model = AdaptiveFactorizationNetwork(field_dims=field_dims, embed_dim=args.embed_dims, LNN_dim=args.LNN_dim,
                                          mlp_dims=(400, 400, 400), dropouts=(0, 0, 0))
 
     criterion = torch.nn.SmoothL1Loss()
     optimizer = torch.optim.Adam(params = model.parameters(), lr = args.lr,
                                  weight_decay=1e-6)
 
+    all_data = pd.read_csv(args.input_csv)
+
+    train_data = all_data[all_data.flag < 8]
+    val_data = all_data[all_data.flag >= 8]
+
+    train_set = TrainDataset(train_data)
+    val_set = TrainDataset(val_data)
+
+    train_loader = DataLoader(train_set, batch_size=args.batch_size)
+    val_loader = DataLoader(val_set, batch_size=args.batch_size)
     
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    for _ in range(args.epochs):
+        train_one_epoch(model, train_loader, criterion, device, optimizer)
+        validate_model(model, val_loader, device)
+        
